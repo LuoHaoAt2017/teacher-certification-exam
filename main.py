6e22bfbce8679ee3e47b9ed9dc9b2503b30c33d6
LuoHao <hluo5206@gmail.com>
2026-05-05 19:05:12 +0800

    docs

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
 #!/usr/bin/env python3
-from scripts.auto_save_idea import watch_clipboard
-
-
-def main() -> int:
-    watch_clipboard()
-
-
-if __name__ == "__main__":
-    raise SystemExit(main())
